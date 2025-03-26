08cec46b6fc86a1ef738c01adf69813a8f053890
tarunmalladi <malladitarun100@gmail.com>
2025-03-26 17:33:12 +0530

    dev folder is added

--- dev/dev.tf
+++ dev/dev.tf
@@ -1,8 +1,9 @@
 module "dev_module" {
-  source                  = "./module"
+  source                  = "../module"
   resource_group_name     = "${var.resource_group_name}-${var.env}"
   resource_group_location = var.resource_group_location
   vnet_name               = var.vnet_name
   vnet_address_space      = var.vnet_address_space
   snet_prefixes           = var.snet_prefixes
+  env="dev"
 }
